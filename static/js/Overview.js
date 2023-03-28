476c0f7fc55eb223248a7f92bb8caa3e9f3f2a08
Vitalii_Kondratskyi <kondratskivd@gmail.com>
2023-03-28 22:55:42 +0300

    fix(SC-OVERVIEW) refactor filters;

--- static/js/Overview.js
+++ static/js/Overview.js
@@ -2,18 +2,10 @@ const OverviewPage = {
     components: {
         'overview-table-cards': OverviewTableCards,
         'overview-chart': OverviewChart,
-        'overview-filter': OverviewFilter
-    }, mounted() {
-        ApiFetchReports().then(data => {
-            this.healthColor = data.application.health
-            this.applicationReports = data.application.reports
-            $('#table_reports_overview').bootstrapTable('load', this.applicationReports);
-        })
-         ApiFetchTests().then(data => {
-            this.securityTests = data
-            $('#table_tests_overview').bootstrapTable('load', this.securityTests);
-        })
-    }, data() {
+        'overview-filter-group': OverviewFilterGroup,
+        'overview-filter-time': OverviewFilterTime,
+    },
+    data() {
         return {
             applicationReports: null,
             securityTests: null,
@@ -33,20 +25,42 @@ const OverviewPage = {
             chartLineOptions,
             chartLineDatasets: [],
             labels: null,
+            defaultGroupsObj: { 'application': true, 'code': true, 'infrastructure': true },
+            selectedGroup: [],
+            selectedTime: {},
         }
-
-    }, watch: {
+    },
+    mounted() {
+        this.initDefaultFilter();
+        ApiFetchReports().then(data => {
+            this.healthColor = data.application.health
+            this.applicationReports = data.application.reports
+            $('#table_reports_overview').bootstrapTable('load', this.applicationReports);
+        })
+         ApiFetchTests().then(data => {
+            this.securityTests = data
+            $('#table_tests_overview').bootstrapTable('load', this.securityTests);
+        })
+    },
+    watch: {
         applicationReports(newValue) {
             if (newValue.length > 0) {
                 this.isLoading = false
                 this.generateDataset(newValue)
             }
         }
-    }, methods: {
+    },
+    methods: {
+        initDefaultFilter() {
+            const d = new Date();
+            d.setUTCDate(d.getUTCDate() - 7);
+            this.selectedTime = { start_time: d.toISOString()}
+            this.selectedGroup = Object.keys(this.defaultGroupsObj);
+        },
         generateDataset(reports) {
             const lineDatasets = [{
                 data: null,
-                label: 'number of runs',
+                label: 'runs',
                 // borderWidth: 2,
                 // borderColor: 'rgb(75, 192, 192)',
                 // backgroundColor: 'rgb(75, 192, 192)',
@@ -66,149 +80,172 @@ const OverviewPage = {
                     occurrences[startDate] += 1
                 }
             });
-            console.log(occurrences)
             this.labels = Object.keys(occurrences).reverse();
             lineDatasets[0].data = Object.values(occurrences).reverse();
             this.chartLineDatasets = lineDatasets;
-
+        },
+        changeGroupFilter(selectedGroup) {
+            this.selectedGroup = selectedGroup;
+            this.refreshReport();
+        },
+        changeTimeFilter(selectedTime) {
+            this.selectedTime = selectedTime;
+            this.refreshReport();
+        },
+        refreshReport() {
+            console.log('new groups', this.selectedGroup);
+            console.log('new time', this.selectedTime)
+            //  ApiFetchReports(this.timeframe).then(data => {
+            //     this.healthColor = data.application.health
+            //     this.applicationReports = data.application.reports
+            //     $('#table_reports_overview').bootstrapTable('load', this.applicationReports);
+            // })
         }
     },
-
     template: `
-    <div class="p-3">
-    <div class="card">
-        <div class="card-header">
-            <h3>Summary</h3>
-        </div>
-        <div class="card-body d-flex flex-column">
-        <overview-filter
-        :data="securityTests"
-        >
-        </overview-filter>
-         <overview-table-cards
-            v-bind:reports="applicationReports"
-            v-bind:healthColor="healthColor"
-            >
-         </overview-table-cards>
-         
-        </div>
-    </div>
-    
-    <div class="card mt-3 p-28">
-        <div class="header">
-            <h4 class="font-bold">Scans frequency</h4>
-        </div>
-            <div>
-                <overview-chart
-                    :key="isLoading"
-                    :is-loading="isLoading"
-                    chart-id="chartScansFrequency"
-                    :options="chartLineOptions"
-                    :datasets="chartLineDatasets"
-                    :labels="labels">
-                </overview-chart>
+        <div class="p-3">
+        <div class="card">
+            <div class="card-header">
+                <h3>Summary</h3>
             </div>
-    </div>
-    
-    <div class="flex-container">
-        <div class="flex-item-2">
-        <TableCard
-                @register="$root.register"
-                instance_name="table_tests_overview"
-                header='Tests'
-                :table_attributes="tests_table_params"
-                :show-custom-count="true"
-                container_classes="my-3"
-                class="table-scroll"
-                :adaptive-height="true"
-            >
-                <template #actions="{master}">
-                <div class="form-group text-right mb-0">
-                    <div class="dropdown dropdown_action">
-                        <button class="btn btn-secondary btn-icon btn-icon__purple mr-2"
-                                role="button"
-                                id="dropdownMenuActionSm"
-                                data-toggle="dropdown"
-                                aria-expanded="false">
-                            <i class="icon__18x18 icon-create-element"></i>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuActionSm">
-                            <li class="px-3 py-1 font-weight-bold">Create Test</li>
-                            <li class="dropdown-item">
-                                <span class="pl-2" data-toggle="modal" data-target="#create_backend_modal_overview">
-                                    Application
-                                </span>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="card-body d-flex flex-column">
+                <div class="d-flex align-items-center">
+                    <overview-filter-group
+                        class="mr-2"
+                        :filter-groups="defaultGroupsObj"
+                        @change-filter="changeGroupFilter">
+                        <template v-slot:label>
+                            <span class="dropdown-toggle_label font-weight-500">GROUP</span>
+                        </template>
+                    </overview-filter-group>
+                    <overview-filter-time
+                        @change-filter="changeTimeFilter"
+                    >
+                    </overview-filter-time>
                 </div>
-                </template>
-                <template #table_headers="{master}">
-                    <th scope="col" data-sortable="true"
-                        data-cell-style="test_formatters.name_style"
-                        data-field="name"
+                <overview-table-cards
+                    v-bind:reports="applicationReports"
+                    v-bind:healthColor="healthColor"
                     >
-                        Name
-                    </th>
-                    <th scope="col" data-sortable="true"
-                        data-cell-style="test_formatters.name_style"
-                        data-field="test_type"
-                    >
-                        Group
-                    </th>
-                    <th scope="col" data-align="right"
-                        data-cell-style="test_formatters.cell_style"
-                        data-formatter=test_formatters.actions
-                        data-events="test_formatters.action_events"
-                    >
-                        Actions
-                    </th>
-                </template>
-            </TableCard>
+                </overview-table-cards>
+            </div>
         </div>
-        <div class="flex-item-2">
+        
+        <div class="card mt-3 p-28">
+            <div class="header">
+                <h4 class="font-bold">Scans frequency</h4>
+            </div>
+                <div>
+                    <overview-chart
+                        :key="isLoading"
+                        :is-loading="isLoading"
+                        chart-id="chartScansFrequency"
+                        :options="chartLineOptions"
+                        :datasets="chartLineDatasets"
+                        :labels="labels">
+                    </overview-chart>
+                </div>
+        </div>
+        
+        <div class="flex-container">
+            <div class="flex-item-2">
             <TableCard
-                @register="$root.register"
-                instance_name="table_reports_overview"
-                header='Latest reports'
-                :table_attributes="reports_table_params"
-                container_classes="my-3"
-                :show-custom-count="true"
-                class="table-scroll"
-                :adaptive-height="true"
-            >
-                <template #actions="{master}">
-                <div class="form-group text-right mb-0">
-                    <button type="button" class="btn btn-secondary btn-icon btn-icon__purple mr-2" 
-                        @click="master.table_action('refresh')">
-                    <i class="fas fa-sync"></i>
-                </button>
-                </div>
-                </template>
-                <template #table_headers="{master}">
-                    <th scope="col" data-checkbox="true"></th>
-                    <th data-visible="false" data-field="id">index</th>
-                    <th scope="col" data-sortable="true" data-field="name"
-                        data-formatter="report_formatters.name"
-                    >
-                        Name
-                    </th>
-                    <th scope="col" data-sortable="true" data-field="start_date"
-                        data-formatter="report_formatters.start"
-                    >
-                        Start
-                    </th>
-                    <th scope="col" data-sortable="true" data-field="test_status"
-                        data-align="right"
-                         data-formatter="report_formatters.full_status"
-                    >
-                        Status
-                    </th>
-                </template>
-            </TableCard>
+                    @register="$root.register"
+                    instance_name="table_tests_overview"
+                    header='Tests'
+                    :table_attributes="tests_table_params"
+                    :show-custom-count="true"
+                    container_classes="my-3"
+                    class="table-scroll"
+                    :adaptive-height="true"
+                >
+                    <template #actions="{master}">
+                    <div class="form-group text-right mb-0">
+                        <div class="dropdown dropdown_action">
+                            <button class="btn btn-secondary btn-icon btn-icon__purple mr-2"
+                                    role="button"
+                                    id="dropdownMenuActionSm"
+                                    data-toggle="dropdown"
+                                    aria-expanded="false">
+                                <i class="icon__18x18 icon-create-element"></i>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuActionSm">
+                                <li class="px-3 py-1 font-weight-bold">Create Test</li>
+                                <li class="dropdown-item">
+                                    <span class="pl-2" data-toggle="modal" data-target="#create_backend_modal_overview">
+                                        Application
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    </template>
+                    <template #table_headers="{master}">
+                        <th scope="col" data-sortable="true"
+                            data-cell-style="test_formatters.name_style"
+                            data-field="name"
+                        >
+                            Name
+                        </th>
+                        <th scope="col" data-sortable="true"
+                            data-cell-style="test_formatters.name_style"
+                            data-field="test_type"
+                        >
+                            Group
+                        </th>
+                        <th scope="col" data-align="right"
+                            data-cell-style="test_formatters.cell_style"
+                            data-formatter=test_formatters.actions
+                            data-events="test_formatters.action_events"
+                        >
+                            Actions
+                        </th>
+                    </template>
+                </TableCard>
+            </div>
+            <div class="flex-item-2">
+                <TableCard
+                    @register="$root.register"
+                    instance_name="table_reports_overview"
+                    header='Latest reports'
+                    :table_attributes="reports_table_params"
+                    container_classes="my-3"
+                    :show-custom-count="true"
+                    class="table-scroll"
+                    :adaptive-height="true"
+                >
+                    <template #actions="{master}">
+                    <div class="form-group text-right mb-0">
+                        <button type="button" class="btn btn-secondary btn-icon btn-icon__purple mr-2" 
+                            @click="master.table_action('refresh')">
+                        <i class="fas fa-sync"></i>
+                    </button>
+                    </div>
+                    </template>
+                    <template #table_headers="{master}">
+                        <th scope="col" data-checkbox="true"></th>
+                        <th data-visible="false" data-field="id">index</th>
+                        <th scope="col" data-sortable="true" data-field="name"
+                            data-formatter="report_formatters.name"
+                        >
+                            Name
+                        </th>
+                        <th scope="col" data-sortable="true" data-field="start_date"
+                            data-formatter="report_formatters.start"
+                        >
+                            Start
+                        </th>
+                        <th scope="col" data-sortable="true" data-field="test_status"
+                            data-align="right"
+                             data-formatter="report_formatters.full_status"
+                        >
+                            Status
+                        </th>
+                    </template>
+                </TableCard>
+            </div>
         </div>
     </div>
-</div>
     `
 }
 register_component("OverviewPage", OverviewPage)
